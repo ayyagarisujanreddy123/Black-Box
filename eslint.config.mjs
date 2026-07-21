458c3ed9f7ca2e2122888668cb70bf6ef6c63c1a
Sujanreddy <Sujanreddy298@gmail.com>
2026-07-20 20:43:36 -0700

    build: exclude disposable demo output from lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default tseslint.config(
     ignores: [
       "**/dist/**",
       "**/node_modules/**",
+      ".blackbox-demo/**",
       "coverage/**",
       "demo/rogue-repo-template/**",
     ],
